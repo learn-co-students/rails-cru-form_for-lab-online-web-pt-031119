54c727ae9f54dfaaf7f55ab4c535f4d452a1b7da
Ryan Pethel <arpethel@gmail.com>
2019-07-28 01:47:13 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # resources :artists, :songs, :genres
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
-  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists, :songs, :genres
+  # resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :genres, only: [:index, :show, :new, :create, :edit, :update]
 end
