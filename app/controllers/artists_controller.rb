b1b1f67173b5a874462d9b8577fc267a3f4043b9
github username <github email address>
2019-07-29 11:32:22 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,46 @@
+class ArtistsController < ApplicationController
+    def new
+        new_artist
+    end 
+
+    def create
+        new_artist
+        update_artist
+        artist_redirect
+    end
+
+    def show
+        find_artist
+    end
+
+    def edit
+        find_artist
+    end
+     
+    def update
+        find_artist
+        update_artist
+        artist_redirect
+    end
+
+    private
+        def artist_params
+            params.require(:artist).permit(:name, :bio)
+        end
+
+        def new_artist
+            @artist = Artist.new
+        end 
+
+        def find_artist
+            @artist = Artist.find(params[:id])
+        end
+
+        def update_artist
+            @artist.update(artist_params)
+        end
+
+        def artist_redirect
+            redirect_to artist_path(@artist)
+        end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,50 @@
+class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+    end
+
+    def new
+        new_song
+    end 
+
+    def create
+        new_song
+        update_song
+        song_redirect
+    end
+
+    def show
+        find_song
+    end
+
+    def edit
+        find_song
+    end
+     
+    def update
+        find_song
+        update_song
+        song_redirect
+    end
+
+    private
+        def song_params
+            params.require(:song).permit(:name, :artist_id, :genre_id)
+        end
+
+        def new_song
+            @song = Song.new
+        end
+
+        def find_song
+            @song = Song.find (params[:id])
+        end
+
+        def update_song
+            @song.update(song_params)
+        end
+
+        def song_redirect
+            redirect_to song_path(@song)
+        end
+end
