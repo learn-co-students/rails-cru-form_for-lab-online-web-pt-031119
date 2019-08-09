603c40e168765f9642cdb161b01d078f2540b8c3
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-08-09 16:08:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,9 @@
 class SongsController < ApplicationController
   
+  def index
+    @songs = Song.all
+  end
+  
   def create
     @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
@@ -17,6 +21,8 @@ class SongsController < ApplicationController
   
   def show
     @song = Song.find(params[:id])
+    @genre = Genre.find(@song.genre_id)
+    @artist = Artist.find(@song.artist_id)
   end
   
   def new
